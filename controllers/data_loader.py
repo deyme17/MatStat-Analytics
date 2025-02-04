3f657dc08f4805525eaf0fbeaaa37065722de2e5
dmytro <dmytro19022006@gmail.com>
2025-02-04 10:52:50 +0200

    add auto bins def

--- controllers/data_loader.py
+++ controllers/data_loader.py
@@ -1,4 +1,5 @@
 from PyQt6.QtWidgets import QFileDialog
+from controllers.plot_controller import set_default_bins
 
 def load_data_file(window):
     path, _ = QFileDialog.getOpenFileName(
@@ -10,10 +11,12 @@ def load_data_file(window):
 
     if path:
         data = window.data_model.load_data(path)
+
         if data is not None and not data.empty:
             window.data = data
             window.bins_spinbox.setEnabled(True)
             window.plot_button.setEnabled(True)
+            window.bins_spinbox.setValue(set_default_bins(window.data))
             print(f'File {path} selected successfully')
         else:
             print(f'Failed to load file {path} or file is empty')
--- controllers/plot_controller.py
+++ controllers/plot_controller.py
@@ -1,5 +1,7 @@
 from models.hist_model import Hist
 from controllers.stat_func import variation_series
+import numpy as np
+import math
 
 def plot_histogram(window):
     if window.data is not None and not window.data.empty:
@@ -15,3 +17,16 @@ def plot_histogram(window):
         window.canvas.draw()
     else:
         print("No data loaded or data is empty.")
+
+def set_default_bins(data):
+    bins = 10
+
+    if not data.empty:
+        classes = len(data)
+
+        if classes <= 100:
+            bins = int(classes ** (1 / 2)) if classes % 2 == 1 else int(classes ** (1 / 2)) - 1
+        else:
+            bins = int(classes ** (1 / 3)) if classes % 2 == 1 else int(classes ** (1 / 3)) - 1
+
+    return max(bins, 1)
