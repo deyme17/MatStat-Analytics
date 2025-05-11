723c301d2a7c9397ef76c3b5dbd72f5e32a3828f
dmytro <dmytro19022006@gmail.com>
2025-05-12 01:57:31 +0300

    del win params

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
 import sys
 from PyQt6.QtWidgets import QApplication
-from models.data_model import Data
-from models.data_processor import DataProcessor
 from views.window import Window
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     app.setStyle("Fusion")
 
-    data_model = Data()
-    data_processor = DataProcessor()
-    window = Window(data_model, data_processor)
+    window = Window()
     window.show()
 
     sys.exit(app.exec())
