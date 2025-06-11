9694592924121a7f6b153c45e45ef6266f571bbc
dmytro <dmytro19022006@gmail.com>
2025-06-11 23:45:16 +0300

    refactor

--- factory.py
+++ factory.py
@@ -1,24 +1,49 @@
+# Controllers
+from controllers.analysis_controller.anomaly_controller import AnomalyController
+from controllers.analysis_controller.estimation_controller import ParameterEstimation
+from controllers.analysis_controller.missing_controller import MissingDataController
+from controllers.analysis_controller.simulation_controller import SimulationController
+from controllers.analysis_controller.statistic_controller import StatisticController
+from controllers.data_controllers.data_loader import DataLoadController
 from controllers.data_controllers.data_transform_controller import DataTransformController
 from controllers.data_controllers.data_version_controller import DataVersionController
-from controllers.data_controllers.data_loader import DataLoadController
-from controllers.ui_controllers.ui_state_controller import UIStateController
-from controllers.analysis_controller.anomaly_controller import AnomalyController
-from controllers.analysis_controller.missing_controller import MissingDataController
-from controllers.analysis_controller.statistic_controller import StatisticController
 from controllers.ui_controllers.graph_controller import GraphController
+from controllers.ui_controllers.ui_state_controller import UIStateController
 
+# Models
 from models.data_model import DataModel
 
+# Services
+from services.analysis_services.confidence_assesment import ConfidenceService
+from services.analysis_services.gof_register import GOFService
+from services.analysis_services.stat_tests import TestPerformer
+from services.analysis_services.statistics_service import StatisticsService
 from services.data_services.data_history_manager import DataHistoryManager
 from services.data_services.data_loader_service import DataLoaderService
-from services.analysis_services.missing_service import MissingService
-from services.analysis_services.statistics_service import StatisticsService
-from services.analysis_services.anomaly_service import AnomalyService
 from services.data_services.transformation_service import TransformationService
+from services.preprocessing_services.anomaly_service import AnomalyService
+from services.preprocessing_services.missing_service import MissingService
+from services.simulation.simulation_engine import SimulationService
+from services.simulation.simulation_saver import DataSaver
+from services.ui_services.renderers.stats_renderer import TableRenderer
 from services.ui_services.ui_refresh_service import UIRefreshService
-from services.ui_services.graph_plotter import GraphPlotter
 
+# Views - Tabs
+from PyQt6.QtWidgets import QTabWidget
+from views.tabs.data_processing_tab import DataProcessingTab
+from views.tabs.gof_test_tab import GOFTestTab
+from views.tabs.params_estimation_tab import ParamEstimationTab
+from views.tabs.simulation_tab import SimulationTab
+from views.tabs.stat_table_tab import StatisticTab
+
+# Views - Widgets
+from views.widgets.dpwidgets.anomalywidget import AnomalyWidget
+from views.widgets.dpwidgets.missingwidget import MissingWidget
+from views.widgets.dpwidgets.processdatawidget import ProcessDataWidget
+from views.widgets.hypoteswidgets.ks_panel import KolmogorovSmirnovPanel
+from views.widgets.hypoteswidgets.pearson_panel import PearsonChi2Panel
 from views.widgets.statwidgets.graph_panel import GraphPanel
+from views.widgets.statwidgets.stat_dist_selector import DistributionSelector
 
 
 class Factory:
@@ -27,24 +52,89 @@ class Factory:
         Factory._setup_services(window)
         Factory._setup_controllers(window)
         Factory._setup_graphics(window)
+        Factory._setup_tabs(window)
 
     @staticmethod
     def _setup_services(window):
         window.version_manager = DataHistoryManager()
-        window.refresher = UIRefreshService()
+        window.refresher = UIRefreshService(window)
 
     @staticmethod
     def _setup_controllers(window):
-        window.data_load_controller = DataLoadController(window=window, data_model=DataModel, loader_service=DataLoaderService())
-        window.transform_controller = DataTransformController(window=window, transform_service=TransformationService())
+        window.data_load_controller = DataLoadController(
+                                        window=window, 
+                                        data_model=DataModel, 
+                                        loader_service=DataLoaderService()
+                                        )
+        window.transform_controller = DataTransformController(
+                                        window=window, 
+                                        transform_service=TransformationService()
+                                        )
         window.data_version_controller = DataVersionController(window)
-        window.state_controller = UIStateController(window=window, missing_service=MissingService())
-        window.anomaly_controller = AnomalyController(window=window, anomaly_service=AnomalyService())
-        window.missing_controller = MissingDataController(window=window, missing_service=MissingService())
-        window.stat_controller = StatisticController(window=window, statistic_service=StatisticsService())
+
+        window.missing_controller = MissingDataController(
+                                        window=window, 
+                                        missing_service=MissingService()
+                                        )
+        window.anomaly_controller = AnomalyController(
+                                        window=window, 
+                                        anomaly_service=AnomalyService()
+                                        )
+        window.stat_controller = StatisticController(
+                                        window=window, 
+                                        statistic_service=StatisticsService(),
+                                        stats_renderer=TableRenderer()
+                                        )
+        window.state_controller = UIStateController(
+                                        window=window, 
+                                        missing_service=MissingService()
+                                        )
 
     @staticmethod
     def _setup_graphics(window):
-        window.graph_panel = GraphPanel(window)
-        window.graph_controller = GraphController(panel=window.graph_panel, plotter=GraphPlotter)
-        window.graph_panel.on_dist_change = lambda: window.graph_controller.evaluate_distribution_change()
+        window.graph_controller = GraphController(
+                                        window=window, 
+                                        confidence_service=ConfidenceService()
+                                        ) 
+        
+        graph_panel = GraphPanel(window, dist_selector=DistributionSelector)
+
+        window.graph_panel = graph_panel
+        window.graph_controller.panel = graph_panel
+
+    @staticmethod
+    def _setup_tabs(window):
+        window.data_tab = DataProcessingTab(
+            window,
+            processor_widget=ProcessDataWidget,
+            anomaly_widget=AnomalyWidget,
+            missing_widget=MissingWidget
+        )
+
+        window.stat_tab = StatisticTab()
+
+        window.gof_tab = GOFTestTab(
+            window,
+            gof_service=GOFService(),
+            test_panels=[PearsonChi2Panel, KolmogorovSmirnovPanel]
+        )
+
+        window.sim_tab = SimulationTab(
+            window,
+            simulation_controller=SimulationController(
+                simulation_service=SimulationService(test_performer=TestPerformer()),
+                data_saver=DataSaver(window)
+            )
+        )
+
+        window.est_tab = ParamEstimationTab(
+            window,
+            estimator=ParameterEstimation()
+        )
+
+        window.left_tab_widget = QTabWidget()
+        window.left_tab_widget.addTab(window.data_tab, "Data Processing")
+        window.left_tab_widget.addTab(window.stat_tab, "Statistic")
+        window.left_tab_widget.addTab(window.gof_tab, "Goodness-of-Fit Tests")
+        window.left_tab_widget.addTab(window.sim_tab, "Simulation")
+        window.left_tab_widget.addTab(window.est_tab, "Parameters estimation")
--- views/window.py
+++ views/window.py
@@ -1,22 +1,14 @@
-from PyQt6.QtWidgets import QMainWindow, QVBoxLayout, QWidget, QHBoxLayout, QTabWidget, QMessageBox
+from PyQt6.QtWidgets import QMainWindow, QVBoxLayout, QWidget, QHBoxLayout, QMessageBox
 from PyQt6.QtGui import QIcon, QPalette, QColor
-
-from views.tabs.data_processing_tab import DataProcessingTab
-from views.tabs.stat_table_tab import StatisticTab
-from views.tabs.gof_test_tab import GOFTestTab
-from views.tabs.simulation_tab import SimulationTab
-from views.tabs.params_estimation_tab import ParamEstimationTab
 
 from views.widgets.window_widget import WindowWidgets
 from utils.ui_styles import appStyle
 from factory import Factory
 
-
 class Window(QMainWindow):
     """
     Main application window for the MatStat Analytics tool.
     """
-
     def __init__(self):
         super().__init__()
         self.setWindowTitle('MatStat Analytics')
@@ -26,49 +18,19 @@ class Window(QMainWindow):
         self._init_palette()
         self.setStyleSheet(appStyle)
 
-        # Data model for currently loaded dataset
         self.data_model = None
-
-        # Register controllers and services via Factory
+        self.widgets = WindowWidgets(self)
         Factory.create(self)
 
-        # UI Widget
-        self.widgets = WindowWidgets(self)
-
-        # UI Tabs
-        self._create_tabs()
         self._create_layout()
 
     def _init_palette(self):
-        """
-        Configure the application’s color palette using light tones.
-        """
         palette = self.palette()
         palette.setColor(QPalette.ColorRole.Window, QColor(240, 248, 255))
         palette.setColor(QPalette.ColorRole.Base, QColor(245, 250, 255))
         self.setPalette(palette)
 
-    def _create_tabs(self):
-        """
-        Create and add the main application tabs.
-        """
-        self.left_tab_widget = QTabWidget()
-        self.data_tab = DataProcessingTab(self)
-        self.stat_tab = StatisticTab()
-        self.gof_tab = GOFTestTab(self)
-        self.sim_tab = SimulationTab(self)
-        self.est_tab = ParamEstimationTab(self)
-
-        self.left_tab_widget.addTab(self.data_tab, "Data Processing")
-        self.left_tab_widget.addTab(self.stat_tab, "Statistic")
-        self.left_tab_widget.addTab(self.gof_tab, "Goodness-of-Fit Tests")
-        self.left_tab_widget.addTab(self.sim_tab, "Simulation")
-        self.left_tab_widget.addTab(self.est_tab, "Parameters estimation")
-
     def _create_layout(self):
-        """
-        Define and apply the main layout including tabs, graph panel and controls.
-        """
         main_panel = QHBoxLayout()
         main_panel.addWidget(self.left_tab_widget, stretch=1)
         main_panel.addWidget(self.graph_panel, stretch=3)
@@ -91,19 +53,10 @@ class Window(QMainWindow):
         self.setCentralWidget(central_widget)
 
     def _create_nav_layout(self):
-        """
-        Proxy method to create a navigation layout from widget utilities.
-        """
         return self.widgets.create_nav_layout()
 
     def show_error_message(self, title, message):
-        """
-        Display an error popup with the given title and message.
-        """
         QMessageBox.critical(self, title, message)
 
     def show_info_message(self, title, message):
-        """
-        Display an informational popup with the given title and message.
-        """
         QMessageBox.information(self, title, message)
