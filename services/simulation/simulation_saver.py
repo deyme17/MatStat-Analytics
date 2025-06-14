82a31a1bf53e56ee08bdf43fc05ba3db6dab238f
dmytro <dmytro19022006@gmail.com>
2025-06-14 13:07:55 +0300

    callback for save

--- services/simulation/simulation_saver.py
+++ services/simulation/simulation_saver.py
@@ -1,16 +1,20 @@
 import numpy as np
 import pandas as pd
+from typing import Callable
 from models.data_model import DataModel
 from utils.def_bins import get_default_bin_count
 
 
 class DataSaver:
-    def __init__(self, window):
+    def __init__(self, context, on_save: Callable[[pd.Series], None]):
         """
-        :param window: Main window instance for accessing version_manager and showing messages
+        Args:
+            context (AppContext): Application context container
+            on_save: Callback to applying data saving
         """
-        self.window = window
+        self.context = context
         self.simulation_counter = {}
+        self.on_save = on_save
         
     def save_data(self, dist_name: str, data: np.ndarray):
         """
@@ -27,12 +31,14 @@ class DataSaver:
         data_model = DataModel(series, bins=optimal_bins, label=dataset_label)
 
         # Add to version manager and update window state
-        self.window.version_manager.add_dataset(dataset_label, data_model)
-        self.window.data_model = data_model
-        self.window.state_controller.handle_post_load_state(series)
+        self.context.version_manager.add_dataset(dataset_label, data_model)
+        self.context.data_model = data_model
+
+        if self.on_save:
+            self.on_save(series)
         
         # success message
-        self.window.show_info_message(
+        self.context.messanger.show_info(
             "Data Saved", 
             f"Simulated data saved as '{dataset_label}' with {len(data)} samples."
         )
