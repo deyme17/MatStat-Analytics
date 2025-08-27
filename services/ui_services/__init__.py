40370772940dba8dbcedd49c257bb21ae5640260
dmytro <dmytro19022006@gmail.com>
2025-08-27 16:24:13 +0300

    TableRenderer

--- services/ui_services/__init__.py
+++ services/ui_services/__init__.py
@@ -1,3 +1,4 @@
 from .messager import UIMessager
 from .missing_info_display import MissingInfoDisplayService
 from .ui_refresh_service import UIRefreshService
+from .renderers.stats_renderer import TableRenderer
