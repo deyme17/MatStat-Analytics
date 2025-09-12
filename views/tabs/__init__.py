7d43d121b865bc03b88c4979df5a307845f8ddc0
dmytro <dmytro19022006@gmail.com>
2025-09-12 22:58:48 +0300

    rename file

--- views/tabs/__init__.py
+++ views/tabs/__init__.py
@@ -2,5 +2,5 @@ from .data_processing_tab import DataProcessingTab
 from .gof_test_tab import GOFTestTab
 from .params_estimation_tab import ParamEstimationTab
 from .simulation_tab import SimulationTab
-from .stat_table_tab import StatisticTab
+from .statistic_tab import StatisticTab
 from .homogen_test_tab import HomogenTab
