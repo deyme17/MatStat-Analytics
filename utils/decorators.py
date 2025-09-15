754ac1197a6e20449f557accb200a1389019b58f
dmytro <dmytro19022006@gmail.com>
2025-09-15 19:23:19 +0300

    new decorator

--- utils/decorators.py
+++ utils/decorators.py
@@ -1,5 +1,6 @@
 import functools
 import pandas as pd
+from models.data_model import DataModel
 
 
 def require_one_dimensional_dataframe(method):
@@ -27,4 +28,30 @@ def require_one_dimensional_dataframe(method):
                 raise RuntimeError("Expected a 1D dataframe (single column).")
 
         return method(self, *args, **kwargs)
+    return wrapper
+
+def require_n_samples(method, n: int | None = None):
+    """
+    Decorator to ensure that dataset list has exactly N samples.
+    If n is None, requires at least 3 sample.
+    """
+    @functools.wraps(method)
+    def wrapper(self, selected_models: dict[str, DataModel], *args, **kwargs):
+        count = len(selected_models)
+        if n is not None:
+            if count != n:
+                self.messanger.show_error(
+                    "Test running error",
+                    f"This test requires exactly {n} datasets, but {count} were selected."
+                )
+                return
+        else:
+            if count < 3:
+                self.messanger.show_error(
+                    "Test running error",
+                    "Please select at least three dataset for this method."
+                )
+                return
+
+        return method(self, selected_models, *args, **kwargs)
     return wrapper
