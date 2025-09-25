7011d8e0153085892c4547b2214e8efcc73a82a0
dmytro <dmytro19022006@gmail.com>
2025-09-25 18:03:09 +0300

    fix text

--- views/widgets/homogenwidgets/n_samples_panels/burtlett_panel.py
+++ views/widgets/homogenwidgets/n_samples_panels/burtlett_panel.py
@@ -8,7 +8,7 @@ class BurtlettPanel(BaseHomoTestPanel):
     def __init__(self, homogen_controller):
         stats = [
             {"key": "S2", "label": "Intergroup Variation"},
-            {"key": "chi2_statistic", "label": "Degrees of freedom"},
+            {"key": "chi2_statistic", "label": "χ²-statistic"},
             {"key": "chi2_crit", "label": "Critical χ²"},
             {"key": "p_value", "label": "P-value"}
         ]
