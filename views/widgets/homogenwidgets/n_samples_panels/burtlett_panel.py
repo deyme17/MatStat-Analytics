b6b42bec7253d0beb12e1bec1a0c55f08201efb2
dmytro <dmytro19022006@gmail.com>
2025-09-25 18:22:59 +0300

    Total Variation

--- views/widgets/homogenwidgets/n_samples_panels/burtlett_panel.py
+++ views/widgets/homogenwidgets/n_samples_panels/burtlett_panel.py
@@ -7,7 +7,7 @@ class BurtlettPanel(BaseHomoTestPanel):
     """
     def __init__(self, homogen_controller):
         stats = [
-            {"key": "S2", "label": "Intergroup Variation"},
+            {"key": "S2", "label": "Total Variation"},
             {"key": "chi2_statistic", "label": "χ²-statistic"},
             {"key": "chi2_crit", "label": "Critical χ²"},
             {"key": "p_value", "label": "P-value"}
