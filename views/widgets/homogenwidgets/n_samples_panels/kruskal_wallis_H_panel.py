2a9339121ececf4c7ede73f4a2df5a7379cd7bd7
dmytro <dmytro19022006@gmail.com>
2025-09-25 17:48:32 +0300

    HPanel

--- views/widgets/homogenwidgets/n_samples_panels/kruskal_wallis_H_panel.py
+++ views/widgets/homogenwidgets/n_samples_panels/kruskal_wallis_H_panel.py
@@ -2,12 +2,15 @@ from views.widgets.homogenwidgets.homogen_panel import BaseHomoTestPanel
 
 class HPanel(BaseHomoTestPanel):
     """
-    Panel for H test.
+    Panel for Kruskal-Wallis H test.
     Displays test statistic, p-value, and decision.
     """
     def __init__(self, homogen_controller):
         stats = [
-            {"key": "", "label": ""},
+            {"key": "H_statistic", "label": "H-statistic"},
+            {"key": "df", "label": "Degrees of freedom"},
+            {"key": "xi2_crit", "label": "Critical χ²"}, 
+            {"key": "p_value", "label": "P-value"}
         ]
         super().__init__(homogen_controller, stats, None, True)
 
