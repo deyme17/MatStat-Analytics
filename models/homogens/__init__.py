12b1bd2985225c8afcccf60e62b2b59928721583
dmytro <dmytro19022006@gmail.com>
2025-09-25 15:02:30 +0300

    mini restructure

--- models/homogens/__init__.py
+++ models/homogens/__init__.py
@@ -1,9 +1,9 @@
-from .normal_homogen_test import NormalHomogenTest
-from .wilcoxon_test import WilcoxonTest
-from .mann_whitney_U_test import MannWhitneyUTest
-from .rank_mean_diff_test import RankMeanDiffTest
-from .smirnov_kolmogorov_test import SmirnovKolmogorovTest
-from .signs_criterion_test import SignsCriterionTest
+from .two_samples_tests.normal_homogen_test import NormalHomogenTest
+from .two_samples_tests.wilcoxon_test import WilcoxonTest
+from .two_samples_tests.mann_whitney_U_test import MannWhitneyUTest
+from .two_samples_tests.rank_mean_diff_test import RankMeanDiffTest
+from .two_samples_tests.smirnov_kolmogorov_test import SmirnovKolmogorovTest
+from .two_samples_tests.signs_criterion_test import SignsCriterionTest
 
 homogens_tests = [
     NormalHomogenTest,
