f7e3232710e8df11922adab038381e698776bf1e
dmytro <dmytro19022006@gmail.com>
2025-09-25 17:56:12 +0300

    xi2 -> chi2

--- views/widgets/homogenwidgets/n_samples_panels/kruskal_wallis_H_panel.py
+++ views/widgets/homogenwidgets/n_samples_panels/kruskal_wallis_H_panel.py
@@ -9,7 +9,7 @@ class HPanel(BaseHomoTestPanel):
         stats = [
             {"key": "H_statistic", "label": "H-statistic"},
             {"key": "df", "label": "Degrees of freedom"},
-            {"key": "xi2_crit", "label": "Critical χ²"}, 
+            {"key": "chi2_crit", "label": "Critical χ²"}, 
             {"key": "p_value", "label": "P-value"}
         ]
         super().__init__(homogen_controller, stats, None, True)
