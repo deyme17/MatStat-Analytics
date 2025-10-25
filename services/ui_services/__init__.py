60f80d58704fc282d2b7af074ec315462d0ea6d5
dmytro <dmytro19022006@gmail.com>
2025-10-25 14:08:32 +0300

    fix init

--- services/ui_services/__init__.py
+++ services/ui_services/__init__.py
@@ -1,5 +1,4 @@
 from .messager import UIMessager
 from .missing_info_display import MissingInfoDisplayService
-from .ui_refresh_service import UIRefreshService
 from .renderers.table_renderers.stats_renderer import StatsRenderer
 from .renderers.table_renderers.var_series_renderer import VarSerRenderer
