6e5d49d39df20a9810adcf59056fe13d38557965
dmytro <dmytro19022006@gmail.com>
2025-10-25 18:07:09 +0300

    exporter support Dataframe too

--- services/data_services/data_exporter.py
+++ services/data_services/data_exporter.py
@@ -24,29 +24,35 @@ class CSVExporter(IExporter):
 
 class DataExporter:
     @classmethod
-    def export(cls, dist_name: str, data: np.ndarray, exporter_cls: IExporter = CSVExporter, out_dir: str = "data/simulated_data") -> str:
+    def export(cls, name: str, data: np.ndarray|pd.DataFrame, exporter_cls: IExporter = CSVExporter, out_dir: str = "data/simulated_data") -> str:
         """
         Export data to file
         Args:
-            dist_name: distribution name
+            name: data name
             data: ndarray (1D or 2D)
             exporter_cls: class for data exporting
             out_dir: directory to save file
         Returns:
             str: path to saved file
         """
-        if data.ndim == 1:
-            df = pd.DataFrame(data, columns=['value'])
-            n_cols = 1
-        elif data.ndim == 2:
-            df = pd.DataFrame(data, columns=[f'col{i+1}' for i in range(data.shape[1])])
+        if isinstance(data, pd.DataFrame):
+            df = data
             n_cols = data.shape[1]
+        elif isinstance(data, np.ndarray):
+            if data.ndim == 1:
+                df = pd.DataFrame(data, columns=['value'])
+                n_cols = 1
+            elif data.ndim == 2:
+                df = pd.DataFrame(data, columns=[f'col{i+1}' for i in range(data.shape[1])])
+                n_cols = data.shape[1]
+            else:
+                raise ValueError(f"Unsupported array dimensionality: {data.ndim}D. Only 1D and 2D arrays are supported.")
         else:
-            raise ValueError(f"Unsupported data dimensionality: {data.ndim}D. Only 1D and 2D arrays are supported.")
+            raise TypeError("Data must be either a pandas DataFrame or numpy ndarray.")
         
         buffer = exporter_cls.export(df)
 
-        filename = f"{dist_name.lower()}{n_cols}_{len(df)}.{exporter_cls.extension}"
+        filename = f"{name.lower()}{n_cols}_{len(df)}.{exporter_cls.extension}"
         filepath = os.path.join(out_dir, filename)
 
         os.makedirs(out_dir, exist_ok=True)
