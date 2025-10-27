87e0207288516f143dfac740e6843a458ccb16ac
dmytro <dmytro19022006@gmail.com>
2025-10-27 17:14:42 +0200

    inits

--- services/ui_services/renderers/graph_renderers/__init__.py
+++ services/ui_services/renderers/graph_renderers/__init__.py
@@ -1,9 +1,15 @@
-from services.ui_services.renderers.graph_renderers.hist_renderer import HistRenderer
-from services.ui_services.renderers.graph_renderers.edf_renderer import EDFRenderer
-from services.ui_services.renderers.graph_renderers.dist_renderer import DistributionRenderer
+from .hist_renderer import HistRenderer
+from .edf_renderer import EDFRenderer
+from .dist_renderer import DistributionRenderer
+
+from .graph2var_renderers.corr_field_renderer import CorrelationFieldRenderer
+from .graph2var_renderers.hist3D_renderer import Histogram3DRenderer
 
 RENDERERS = {
     'histogram': HistRenderer,
     'edf': EDFRenderer,
-    'distribution': DistributionRenderer
+    'distribution': DistributionRenderer,
+
+    'correlation_field': CorrelationFieldRenderer,
+    'histogram3D': Histogram3DRenderer
 }
---
+++ views/widgets/statwidgets/graph_tabs/two_vars_graph_tabs/__init__.py
@@ -0,0 +1,2 @@
+from .corr_field_tab import CorrilationFieldTab
+from .hist3D_tab import Hist3DTab
