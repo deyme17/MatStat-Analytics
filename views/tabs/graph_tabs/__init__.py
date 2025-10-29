c9aa71b1299e633a2f9844bc3aed18968468b080
dmytro <dmytro19022006@gmail.com>
2025-10-29 10:48:38 +0200

    remove graph tabs to tabs

---
+++ views/tabs/graph_tabs/__init__.py
@@ -0,0 +1,4 @@
+from .hist_tab import HistogramTab
+from .edf_tab import EDFTab
+from .two_vars_graph_tabs.corr_field_tab import CorrelationFieldTab
+from .two_vars_graph_tabs.histMap_tab import HistogramMapTab
