49e86e2ae34650213dabc3cc2ab97e5a836d10eb
dmytro <dmytro19022006@gmail.com>
2025-11-02 01:04:30 +0200

    inits

--- models/gofs/__init__.py
+++ models/gofs/__init__.py
@@ -1,8 +1,10 @@
 from .chi2_test import ChiSquaredGOFTest
+from .chi2_2d_test import ChiSquared2DGOFTest
 from .ks_test import KolmogorovSmirnovGOFTest
 from .base_gof_test import BaseGOFTest
 
 gof_tests: list[BaseGOFTest] = [
     KolmogorovSmirnovGOFTest,
-    ChiSquaredGOFTest
+    ChiSquaredGOFTest,
+    ChiSquared2DGOFTest
 ]
--- views/widgets/gofwidgets/__init__.py
+++ views/widgets/gofwidgets/__init__.py
@@ -1,2 +1,3 @@
 from .ks_panel import KolmogorovSmirnovPanel
 from .pearson_panel import PearsonChi2Panel
+from .pearson2D_panel import Pearson2DChi2Panel
