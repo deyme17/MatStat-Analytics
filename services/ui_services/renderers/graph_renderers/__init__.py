73073b1ab7a2b2e27c25dece4301e270f62724f9
dmytro <dmytro19022006@gmail.com>
2025-11-03 14:40:08 +0200

    CorrMatrixRenderer

--- services/ui_services/renderers/graph_renderers/__init__.py
+++ services/ui_services/renderers/graph_renderers/__init__.py
@@ -2,6 +2,7 @@ from .hist_renderer import HistRenderer
 from .edf_renderer import EDFRenderer
 from .dist_renderer import DistributionRenderer
 from .hh_renderer import HHRenderer
+from .corr_matrix_renderer import CorrMatrixRenderer
 
 from .graph2var_renderers.corr_field_renderer import CorrelationFieldRenderer
 from .graph2var_renderers.histMap_renderer import HistogramMapRenderer
@@ -13,6 +14,7 @@ RENDERERS: dict[str, type[Renderer]] = {
     'edf': EDFRenderer,
     'distribution': DistributionRenderer,
     'hh_plot': HHRenderer,
+    'correlation_matrix': CorrMatrixRenderer,
 
     'correlation_field': CorrelationFieldRenderer,
     'histogram_map': HistogramMapRenderer
---
+++ services/ui_services/renderers/graph_renderers/corr_matrix_renderer.py
@@ -0,0 +1,37 @@
+import seaborn as sns
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from services.ui_services.renderers.graph_renderers.graph_renderer import Renderer
+
+class CorrMatrixRenderer(Renderer):
+    """
+    Renderer for drawing correlation matrices using seaborn heatmap.
+    """
+    @staticmethod
+    def render(ax: plt.Axes, data: pd.DataFrame, corr_callable: callable[[pd.Series, pd.Series], float], 
+               corr_name: str = None, annot: bool = True, cmap: str = "coolwarm"):
+        """
+        Render correlation matrix on the given Matplotlib axis.
+        Args:
+            ax (plt.Axes): Matplotlib axis to draw the heatmap on.
+            data (pd.DataFrame): Input dataframe.
+            corr_callable (callable): Function to compute correlation between two arrays.
+                                      Should accept (x: pd.Series, y: pd.Series) and return float.
+            corr_name (str): Name of the correlation coefficient
+            annot (bool): Whether to annotate the heatmap with coefficient values.
+            cmap (str): Color map to use for heatmap.
+        """
+        columns = data.columns
+        n = len(columns)
+        corr_matrix = np.zeros((n, n))
+
+        # correlation matrix
+        for i, col1 in enumerate(columns):
+            for j, col2 in enumerate(columns):
+                corr_matrix[i, j] = corr_callable(data[col1], data[col2])
+
+        corr_df = pd.DataFrame(corr_matrix, index=columns, columns=columns)
+        sns.heatmap(corr_df, annot=annot, cmap=cmap, ax=ax, vmin=-1, vmax=1)
+        title = "Correlation Matrix" + (f"({corr_name})" if corr_name else '')
+        ax.set_title(title)
