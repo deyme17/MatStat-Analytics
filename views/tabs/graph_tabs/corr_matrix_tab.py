091aaa29e0b95d0282f9a31cc82aac2b9ff5238b
dmytro <dmytro19022006@gmail.com>
2025-11-03 17:33:12 +0200

    EventBus in CorrelationMatrixTab

--- views/tabs/graph_tabs/corr_matrix_tab.py
+++ views/tabs/graph_tabs/corr_matrix_tab.py
@@ -2,7 +2,7 @@ from .graph_tab import BaseGraphTab
 from typing import Callable
 from controllers import CorrelationController
 from services.ui_services.renderers.graph_renderers import RENDERERS
-from utils import AppContext
+from utils import AppContext, EventBus, EventType, Event
 
 
 class CorrelationMatrixTab(BaseGraphTab):
@@ -12,14 +12,25 @@ class CorrelationMatrixTab(BaseGraphTab):
         super().__init__(name="Correlation Matrix", context=context)
         self.controller: CorrelationController = controller
         self.get_corr_coef: Callable[[None], str] = get_corr_coef
+        self.event_bus: EventBus = context.event_bus
+        self._subscribe_to_events()
+
+    def _subscribe_to_events(self):
+        """Subscribe to relevant events."""
+        self.event_bus.subscribe(EventType.CORR_COEFF_CHANGED, self._on_corr_changed)
+
+    def _on_corr_changed(self, event: Event) -> None:
+        """Handle rendering parameter changes."""
+        corr_coef_name = event.data
+        self.draw(corr_coef_name)
         
-    def draw(self):
+    def draw(self, corr_coef_name: str = None):
         """Draw correlation matrix using selected correlation coeficient"""
         self.clear()
         if self.panel is None: return
             
         dataframe = self.context.data_model.dataframe
-        corr_coef_name = self.get_corr_coef()
+        corr_coef_name = self.get_corr_coef() if not corr_coef_name else corr_coef_name
         
         # Render corr matrix
         renderer = RENDERERS['correlation_matrix']
@@ -30,5 +41,5 @@ class CorrelationMatrixTab(BaseGraphTab):
             corr_coef_name,
         )
         # styling
-        self.apply_default_style(self.ax, "Value", "Frequency")
+        self.apply_default_style(self.ax, "X", "Y")
         self.canvas.draw()
