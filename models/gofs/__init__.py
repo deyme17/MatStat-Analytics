39e050147fb14425a2510c1469091766244245e3
dmytro <dmytro19022006@gmail.com>
2025-11-03 14:06:52 +0200

    0

--- models/gofs/__init__.py
+++ models/gofs/__init__.py
@@ -3,7 +3,7 @@ from .ks_test import KolmogorovSmirnovGOFTest
 from .normal2d_chi2_test import Normal2DChi2GOFTest
 from .base_gof_test import BaseGOFTest
 
-gof_tests: list[BaseGOFTest] = [
+gof_tests: list[type[BaseGOFTest]] = [
     KolmogorovSmirnovGOFTest,
     ChiSquaredGOFTest,
     Normal2DChi2GOFTest,
