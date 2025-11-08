5d8b1d7d694bc4f121a14ec6728772ba1cf4e234
dmytro <dmytro19022006@gmail.com>
2025-11-08 17:10:12 +0200

    init update

--- controllers/analysis_controller/__init__.py
+++ controllers/analysis_controller/__init__.py
@@ -5,3 +5,4 @@ from .gof_controller import GOFController
 from .homogen_controller import HomogenController
 from .correlation_controller import CorrelationController
 from .dist_register import DistributionRegister
+from .regression_controller import RegressionController
