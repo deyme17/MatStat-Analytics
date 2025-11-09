8fbaa4a674a60b682b5ea160edb6b99bbb83d504
dmytro <dmytro19022006@gmail.com>
2025-11-09 16:52:38 +0200

    update inits

--- models/regression/__init__.py
+++ models/regression/__init__.py
@@ -1,7 +1,6 @@
-from .regression_model import IRegression
-from .linear_regression import LinearRegression
-
-from services import OLS
+from .interfaces import IRegression
+from .regression_models import LinearRegression
+from .algorithms import OLS
 
 regression_models: list[IRegression] = [
     LinearRegression(algorithm=OLS())
