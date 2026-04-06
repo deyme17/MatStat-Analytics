6dcc1341c9a86f563781fe3e489b4c5a6af6b4d6
dmytro <dmytro19022006@gmail.com>
2026-04-06 16:56:57 +0300

    restructure

--- services/ui_services/renderers/graph_renderers/__init__.py
+++ services/ui_services/renderers/graph_renderers/__init__.py
@@ -1,10 +1,7 @@
-from .hist_renderer import HistRenderer
-from .edf_renderer import EDFRenderer
-from .dist_renderer import DistributionRenderer
-from .hh_renderer import HHRenderer
-
-from .reg_plot_dialog_renderer import RegressionPlotDialog
-from .resfitted_plot_renderer import ResidualsFittedPlot
+from .graph1var_renderers.hist_renderer import HistRenderer
+from .graph1var_renderers.edf_renderer import EDFRenderer
+from .graph1var_renderers.dist_renderer import DistributionRenderer
+from .graph1var_renderers.hh_renderer import HHRenderer
 
 from .graph2var_renderers.corr_field_renderer import CorrelationFieldRenderer
 from .graph2var_renderers.histMap_renderer import HistogramMapRenderer
@@ -13,6 +10,9 @@ from .graph3var_renderers.bubble_plot_renderer import BubblePlotRenderer
 from .graphMultivar_renderers.corr_matrix_renderer import CorrMatrixRenderer
 from .graphMultivar_renderers.heatmap_renderer import HeatMapRenderer
 from .graphMultivar_renderers.parallel_coord_renderer import ParallelCoordsRenderer
+
+from .reg_plot_dialog_renderer import RegressionPlotDialog
+from .resfitted_plot_renderer import ResidualsFittedPlot
 
 from .graph_renderer import Renderer
 
