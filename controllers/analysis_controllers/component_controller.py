6ba2e4520a068f7c527547a192476e79f1ad26f9
dmytro <dmytro19022006@gmail.com>
2026-04-28 19:28:27 +0300

    add separate method `fit` and *fit flag* in `transform`

--- controllers/analysis_controllers/component_controller.py
+++ controllers/analysis_controllers/component_controller.py
@@ -12,22 +12,39 @@ class ComponentController:
         self.orig_X_df_ = Optional[pd.DataFrame] = None
         self.pca_labels: Optional[List[str]] = None
 
-    def fit_transform(self, X_df: pd.DataFrame,
-                      n_components: Optional[int] = None,
-                      ev_threshold: Optional[float] = None) -> pd.DataFrame:
+    def fit(self, X_df: pd.DataFrame) -> None:
+        """
+        Fit the PCA model to the data.
+        Args:
+            X (np.ndarray): Data of shape (n_samples, n_features).
+        """
+        X = X_df.to_numpy(dtype=float)
+        self.pca.fit(X)
+
+    def transform(self, X_df: pd.DataFrame,
+                 fit: bool = True,
+                 n_components: Optional[int] = None,
+                 ev_threshold: Optional[float] = None) -> pd.DataFrame:
         """
         Tranform X dataframe using PCA model.
         Args:
             X_df (pd.DataFrame): Dataframe of shape (n_samples, n_features).
+            fit (bool): Fit the PCA model before transform.
             n_components (int|None): Number of principal components.
             evr_threshold (float|None): Explained variance threshold,
                     that is used for principal components selection.
         Returns:
             pd.DataFrame: Transformed dataframe of shape (n_samples, n_components).
         """
-        self.orig_X_df_ = X_df
         X = X_df.to_numpy(dtype=float)
-        X_transformed = self.pca.fit_transform(X, n_components, ev_threshold)
+
+        X_transformed = None
+        if fit:
+            X_transformed = self.pca.fit_transform(X, n_components, ev_threshold)
+        else:
+            X_transformed = self.pca.transform(X, n_components, ev_threshold)
+
+        self.orig_X_df_ = X_df
         self.pca_labels = [f"PC_{i+1}" for i in range(X_transformed.shape[1])]
         return pd.DataFrame(X_transformed, columns=self.pca_labels)
     
